05f0fe1c87c0b13abef45594cd025e0fe04df4b6
MCNGN <rifqi.adibbahagia@binus.ac.id>
2024-12-22 20:08:36 +0700

    Add Forum
    Add pages forum and create dummy data for testing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,13 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      height: {
+        120: '120px',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
