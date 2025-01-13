2f40459cc439c079fa2d0ee825b60a382157fc2b
MCNGN <rifqi.adibbahagia@binus.ac.id>
2025-01-13 16:52:03 +0700

    Add Comment Logic and Add API

--- src/app/forum/[id]/page.tsx
+++ src/app/forum/[id]/page.tsx
@@ -54,6 +54,20 @@ export default function ForumDetail() {
   const params = useParams();
   const [forumData, setForumData] = useState<PostData | null>(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [comment, setComment] = useState("");
+
+  interface CommentData {
+    id: string;
+    profile: string;
+    comment: string;
+  }
+
+  const [comments, setComments] = useState<CommentData[]>([]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setComment(e.target.value);
+  };
+
   useEffect(() => {
     const checkLoginStatus = () => {
       const userCookie = Cookies.get("isLoggedIn");
@@ -65,32 +79,83 @@ export default function ForumDetail() {
     checkLoginStatus();
   }, []);
 
+  const handlePostComment = async () => {
+    if (comment.length === 0) return;
+    const userId = Cookies.get("id");
+
+    try {
+      const response = await fetch(
+        `https://budaya-karya-kita-backend.vercel.app/comment/`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            post_id: params.id,
+            comment: comment,
+            user_id: userId,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to post comment");
+      }
+
+      setComment(""); // Clear the input field
+      const updatedComments = await fetch(
+        `https://budaya-karya-kita-backend.vercel.app/comment/${params.id}/`
+      );
+      const updatedCommentsJson = await updatedComments.json();
+      setComments(updatedCommentsJson);
+    } catch (error) {
+      console.error("Error posting comment:", error);
+    }
+  };
+
+  const fetchForumData = async () => {
+    try {
+      const response = await fetch(
+        `https://budaya-karya-kita-backend.vercel.app/forum/${params.id}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch forum data");
+      }
+
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        setForumData(jsonResponse);
+      } else {
+        console.error("Failed to fetch:", response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching forum data:", error);
+    }
+  };
+
   useEffect(() => {
-    const fetchForumData = async () => {
+    fetchForumData();
+  });
+
+  useEffect(() => {
+    const fetchComments = async () => {
       try {
         const response = await fetch(
-          `https://budaya-karya-kita-backend.vercel.app/forum/${params.id}`
+          `https://budaya-karya-kita-backend.vercel.app/comment/${params.id}/`
         );
         if (!response.ok) {
-          throw new Error("Failed to fetch forum data");
+          throw new Error("Failed to fetch comments");
         }
 
-        if (response.ok) {
-          const jsonResponse = await response.json();
-          setForumData(jsonResponse);
-        } else {
-          console.error(
-            "Failed to fetch:",
-            response.status,
-            response.statusText
-          );
-        }
+        const jsonResponse = await response.json();
+        setComments(jsonResponse);
       } catch (error) {
-        console.error("Error fetching forum data:", error);
+        console.error("Error fetching comments:", error);
       }
     };
 
-    fetchForumData();
+    fetchComments();
   }, [params.id]);
 
   return (
@@ -115,22 +180,35 @@ export default function ForumDetail() {
                 caption={forumData.caption}
               />
               <div className="relative">
-                <div className="flex flex-row items-start px-4 w-full mb-4">
-                  <div className="w-[30px] h-[30px] rounded-full inline-flex items-center justify-center bg-gray-500 text-gray-700 flex-shrink-0 mr-2 relative">
-                    <Image
-                      src={"https://i.imgur.com/nBk7ymi.jpeg"}
-                      alt=""
-                      width={45}
-                      height={45}
-                      className="shadow-xl border-black rounded-full"
-                    />
+                {comments.length > 0 ? (
+                  comments.map((comment) => (
+                    <div
+                      key={comment.id}
+                      className="flex flex-row items-start px-4 w-full  mb-4 "
+                    >
+                      <div className="flex flex-row items-center">
+                        <div className="w-[30px] h-[30px] rounded-full inline-flex items-center justify-center bg-gray-500 text-gray-700 flex-shrink-0 mr-2 relative">
+                          {comment.profile && (
+                            <Image
+                              src={comment.profile}
+                              alt=""
+                              width={45}
+                              height={45}
+                              className="shadow-xl border-black rounded-full"
+                            />
+                          )}
+                        </div>
+                        <div className="break-word text-xs">
+                          {comment.comment}
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <div className="flex w-full h-[320px] items-center justify-center">
+                    <p>No comments yet.</p>
                   </div>
-                  <div className="break-word text-xs">
-                    Test ini komen Test ini komen Test ini komen Test ini komen
-                    Test ini komen Test ini komen Test ini komen Test ini komen
-                    Test ini komen Test ini komen Test ini komen Test ini komen
-                  </div>
-                </div>
+                )}
 
                 {isLoggedIn && (
                   <div className="absolute top-80 left-0 w-full p-4 bg-white">
@@ -138,12 +216,22 @@ export default function ForumDetail() {
                       type="text"
                       placeholder="Add a comment..."
                       className="w-full px-3 py-2 border rounded-lg focus:outline-none"
+                      value={comment}
+                      onChange={handleInputChange}
                     />
                     <button
                       type="button"
                       className="absolute inset-y-0 right-7 flex items-center "
+                      onSubmit={handlePostComment}
+                      disabled={comment.length === 0}
                     >
-                      <div className="hover:text-gray-500">Post</div>
+                      <div
+                        className={` ${
+                          comment.length === 0 ? "text-gray-400" : ""
+                        }`}
+                      >
+                        Post
+                      </div>
                     </button>
                   </div>
                 )}
